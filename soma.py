3d36652f1c27944088d487f5dd824ebbf257112b
kaue <kaue.11@hotmail.com>
2023-08-17 19:14:50 -0300

    Feito

---
+++ soma.py
@@ -0,0 +1,6 @@
+for c in range(0,3):
+    primeiro_numero = int(input("Digite o primeiro numero\n"))
+    segundo_numero = int(input("Digite o segundo numero\n"))
+    terceiro_numero = int(input("Digite o terceiro numero\n"))
+    soma = primeiro_numero + segundo_numero + terceiro_numero
+    print("O resultado é o  {}".format(soma))
